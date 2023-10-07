dc170e48c3a8ed9229b49b72e71012602bc4211b
NeMoSova19 <ya.ffrg20162016@yandex.ru>
2023-10-08 01:17:13 +0300

    Print

--- main.cpp
+++ main.cpp
@@ -1,8 +1,16 @@
 #include "CPython.h"
+#include <vector>
 using namespace std;
 
 int main() {
-	map<int, int> a = Input(2);
-	Print(a);
+
+	//map<int, int> mii;
+	//mii.insert({ 1, 5 });
+	//mii.insert({ 3, 17 });
+	//mii.insert({ 44, 55 });
+	//
+	//Print p(mii);
+
+	Print::Settings()
 	return 0;
 }
